984e6b658cd48239de85e6ef0047daec35262098
Kevin Li <legendre6891@gmail.com>
2017-01-22 00:43:28 -0500

    Cosmetic changes (removing spaces, fixing typos).

--- lisp/my-functions.el
+++ lisp/my-functions.el
@@ -4,12 +4,12 @@
   (let ((filename (buffer-file-name)))
     (if (not (and filename (file-exists-p filename)))
         (message "Buffer is not visiting a file!")
-        (let ((new-name (read-file-name "New name: " filename)))
-          (cond
-            ((vc-backend filename) (vc-rename-file filename new-name))
-            (t
-             (rename-file filename new-name t)
-             (set-visited-file-name new-name t t)))))))
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+          ((vc-backend filename) (vc-rename-file filename new-name))
+          (t
+           (rename-file filename new-name t)
+           (set-visited-file-name new-name t t)))))))
 
 (global-set-key (kbd "C-c C-r") 'rename-file-and-buffer)
 
